054e97f90bd609a2daa17f0fc272edfb02c484ce
Mustafa <69982652+Kaves47@users.noreply.github.com>
2021-01-07 17:37:03 +0300

    Add files via upload

---
+++ kaves_gunbutt/main.lua
@@ -0,0 +1,12 @@
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+		local ped = PlayerPedId()
+		SetPedSuffersCriticalHits(ped, false)
+        DisableControlAction(0, 140, true)
+        if IsPlayerFreeAiming(PlayerId()) then
+            DisableControlAction(1, 141, true)
+            DisableControlAction(1, 142, true)
+        end
+    end
+end)
