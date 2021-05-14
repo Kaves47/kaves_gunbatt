ffa22bfa85961466277dd685517f8d551df08610
Kaves47 <69982652+Kaves47@users.noreply.github.com>
2021-05-14 23:20:38 +0300

    Update main.lua

--- kaves_gunbutt/main.lua
+++ kaves_gunbutt/main.lua
@@ -1,7 +1,6 @@
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-	local ped = PlayerPedId()
         DisableControlAction(0, 140, true)
         if IsPlayerFreeAiming(PlayerId()) then
             DisableControlAction(1, 141, true)
